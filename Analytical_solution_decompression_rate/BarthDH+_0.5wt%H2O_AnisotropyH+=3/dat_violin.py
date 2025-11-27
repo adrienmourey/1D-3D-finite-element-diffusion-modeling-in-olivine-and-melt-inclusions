822662a7a41176cee87062b9ebf4220c03790daa
Adrien Mourey <79226319+adrienmourey@users.noreply.github.com>
2025-11-27 10:07:49 +0800

    Add files via upload

---
+++ Analytical_solution_decompression_rate/BarthDH+_0.5wt%H2O_AnisotropyH+=3/dat_violin.py
@@ -0,0 +1,35 @@
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import math as m
+import matplotlib.pyplot as plt
+
+# Import dataframe
+
+df = pd.read_csv('Kil_pyMN_dfout.csv')
+
+# Calculete log 10 dpdt 
+
+df['log10 dpdt'] = np.log10(df['dpdt'].values)
+
+
+# Create plot
+plt.figure(figsize=(15, 10))
+sns.violinplot(data = df, x = 'Sample', y = 'log10 dpdt', inner = 'quart', gap = 0.5, linewidth = 0.8)
+
+# set plot area
+
+plt.savefig('Barth_0.5_3_violin.pdf')
+plt.savefig('Barth_0.5_3_violin.png')
+plt.close()
+
+
+# Create KDE of all distributions
+
+sns.kdeplot(data= df, x="log10 dpdt", bw_method = 'silverman', fill = True)
+
+plt.savefig('Barth_0.5_3_kde.pdf')
+plt.savefig('Barth_0.5_3_kde.png')
+plt.close()
---
+++ Analytical_solution_decompression_rate/BarthDH+_0.5wt%H2O_AnisotropyH+=3/dif_inv_MPI_auto.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+Profiles="SW_7_2_1 SW_7_3_1 SW_7_3_2 SW_7_4_1 SW_7_6_1 SW_7_7_1 SW_7_8_1 SW_7_10_1 K97_23_1_1_1 K97_23_4_1 K97_23_5_1 K97_23_8_1 K97_23_10_1 K97_23_16_1 K97_23_16_2 K97_23_16_3 K97_23_17_1" 
+
+
+#Profiles="K97_23_15_1" 
+
+for x in ${Profiles} ; do # Loop through relevant profiles
+
+echo "${x}"
+
+# CHAINS_4
+f_dat="Lerner2024MIs_golden_pumice.csv" # Data file
+f_degas="magma_sat_hawaii_0.5_short.csv" # degassing path 
+f_out="${x}_MI_dpdt" # Name of output folder
+#mv "$f_out"/* ~/.trash
+
+mkdir $f_out
+ 
+mpiexec -n 4 python3 H2O_mi_inversion_analytical.py $f_dat $f_degas ${x} $f_out"/vars_1" # Run on 4 cores
+
+python3 multinest_marginals.py $f_out/vars_1 # Final plotting script
+
+done
+
+
+
+
